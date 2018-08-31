99a2a9621af0b0130da06ca95a5d04c4916fc16a
duartecancela <duartecancela@gmail.com>
2018-08-31 11:51:15 +0100

    Aula 21

--- fundamentos/booleano.js
+++ fundamentos/booleano.js
@@ -16,3 +16,18 @@ console.log(!![])
 console.log(!!{})
 console.log(!!Infinity)
 console.log(!!(isAtivo = true))
+
+// Expresões falsas
+console.log('Os falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log('para finalizar...')
+console.log(!!('' || null || 0 || ' '))
+
+let nome = ''
+console.log(nome || 'Desconhecido')
