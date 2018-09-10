ffedf0dc590858ed10298d5f91865c7e43d2a8a4
Duarte Cancela - ULSBA <20445@ulsba.min-saude.pt>
2018-09-10 17:06:31 +0100

    Seção 6 Aula 86

---
+++ objeto/oo.js
@@ -0,0 +1,21 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valopr1, valor2, valor3)
+
+ // 00
+ objeto = {
+     valor1,
+     valor2,
+     valor3,
+     processamento() {
+         // ...
+     }
+ }
+
+ Objeto.processamento // Foco passou a ser o objeto
+
+// Princípios importantes:
+// 1. abstraçao
+// 2. encapsulamento
+// 3. Herança 
